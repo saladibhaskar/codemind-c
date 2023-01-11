bc8cbd6c4282aea9f82c5be09034226c6267ea00
saladibhaskar <119723167+saladibhaskar@users.noreply.github.com>
2023-01-11 11:22:38 +0530

    #include<stdio.h> #include<string.h> int main() {
        int c=0,i,s;
        char a[100];
        scanf("%[^
    ]s",a);
        printf("Hello Technicalhub
    ");
        for(i=0;a[i]!=NULL;i++)
        {
            printf("%c",a[i]);
        }
        
    }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+    int c=0,i,s;
+    char a[100];
+    scanf("%[^
+]s",a);
+    printf("Hello Technicalhub
+");
+    for(i=0;a[i]!=NULL;i++)
+    {
+        printf("%c",a[i]);
+    }
+    
+}
