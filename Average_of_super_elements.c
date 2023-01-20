e40e526a33dd98c1c54fac146e9d811f369e75aa
saladibhaskar <119723167+saladibhaskar@users.noreply.github.com>
2023-01-20 10:33:08 +0530

    #include<stdio.h> int main() {     long int n,i; 	float sum=0,c=-1;     scanf("%ld",&n);     long int a[n],b[10];     for(i=0;i<n;i++)     {         scanf("%ld",&a[i]);         b[a[i]]++;     }     for(i=0;i<10;i++)     {         if(b[i]==i)         {             sum=sum+i;             c++;         }     }
        if(c>0)
        {
        printf("%.2f",sum/c);
        }
        else
        {
            printf("%d",-1);
        }
        
        
    }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -4,13 +4,13 @@ int main()
     long int n,i;
 	float sum=0,c=-1;
     scanf("%ld",&n);
-    long int a[n],b[11];
+    long int a[n],b[10];
     for(i=0;i<n;i++)
     {
         scanf("%ld",&a[i]);
         b[a[i]]++;
     }
-    for(i=0;i<11;i++)
+    for(i=0;i<10;i++)
     {
         if(b[i]==i)
         {
@@ -19,7 +19,7 @@ int main()
         }
     }
      
-    if(c>0&&sum>0)
+    if(c>0)
     {
     printf("%.2f",sum/c);
     }
